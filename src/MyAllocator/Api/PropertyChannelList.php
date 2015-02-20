e4810fa0fab8708cc2cd81bf0ebb4fcd61e8901c
nathanhelenihi <nathan.helenihi@cloudbeds.com>
2015-02-20 14:22:09 -0800

    fix PropertyChannelList API

--- src/MyAllocator/Api/PropertyChannelList.php
+++ src/MyAllocator/Api/PropertyChannelList.php
@@ -45,6 +45,7 @@ class PropertyChannelList extends MaApi
             'req' => array(
                 'Auth/VendorId',
                 'Auth/VendorPassword',
+                'Auth/PropertyId',
                 'UserCredentials' => array(
                     'Auth/UserId',
                     'Auth/UserPassword'
